d03c28fc7005a8138c34bc43b27245dfa6db455f
LucasSSilvaJs <lucas25th@gmail.com>
2024-11-12 13:25:52 -0300

    Mudanças como merge e outros

--- lib/pages/profileimg.dart
+++ lib/pages/profileimg.dart
@@ -10,17 +10,21 @@ class ProfileImg extends StatefulWidget {
 
 class _ProfileImgState extends State<ProfileImg> {
   File? _imageFile;
+  String? _imageName;
 
   Future<void> _pickImage() async {
     final picker = ImagePicker();
     final pickedFile = await picker.pickImage(
       source: ImageSource.gallery, // Abre a galeria do dispositivo
-      imageQuality: 50, // Reduz a qualidade da imagem para otimizar o tamanho do arquivo
+      imageQuality:
+          50, // Reduz a qualidade da imagem para otimizar o tamanho do arquivo
     );
 
     if (pickedFile != null) {
       setState(() {
-        _imageFile = File(pickedFile.path); // Armazena o caminho da imagem selecionada
+        _imageFile =
+            File(pickedFile.path); // Armazena o caminho da imagem selecionada
+        _imageName = pickedFile.name.replaceAll('scaled_', '');
       });
     }
   }
@@ -42,15 +46,36 @@ class _ProfileImgState extends State<ProfileImg> {
             ),
             child: Column(
               children: [
-                const SizedBox(height: 60,),
+                const SizedBox(
+                  height: 60,
+                ),
                 Container(
                   alignment: Alignment.center,
                   child: Column(
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
-                      const Text('Bem-vindo(a), ', style: TextStyle(fontSize: 40.0, color: Colors.white,),),
-                      const Text('Fulano!', style: TextStyle(fontSize: 40.0, fontWeight: FontWeight.bold, color: Color(0xFFF5A13B)),),
-                      const Text('Adicione um foto de perfil', style: TextStyle(fontSize: 20.0, color: Colors.white, fontWeight: FontWeight.bold,),),
+                      const Text(
+                        'Bem-vindo(a), ',
+                        style: TextStyle(
+                          fontSize: 40.0,
+                          color: Colors.white,
+                        ),
+                      ),
+                      const Text(
+                        'Fulano!',
+                        style: TextStyle(
+                            fontSize: 40.0,
+                            fontWeight: FontWeight.bold,
+                            color: Color(0xFFF5A13B)),
+                      ),
+                      const Text(
+                        'Adicione um foto de perfil',
+                        style: TextStyle(
+                          fontSize: 20.0,
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
                     ],
                   ),
                 ),
@@ -73,7 +98,17 @@ class _ProfileImgState extends State<ProfileImg> {
                     ),
                   ),
                 ),
-                const Text('Nenhum arquivo adicionado'),
+                SizedBox(
+                  height: 10.0,
+                ),
+                Text(
+                  _imageFile != null
+                      ? '$_imageName'
+                      : 'Nenhum arquivo encontrado',
+                  style: TextStyle(
+                    color: Colors.white,
+                  ),
+                ),
                 const SizedBox(height: 25.0),
                 Padding(
                   padding: const EdgeInsets.symmetric(horizontal: 20.0),
